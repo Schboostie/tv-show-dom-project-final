9e50bcfb1fe38d4f6ed83ec8229e1a7ac79dbaee
Shahid Amin <schboostie@gmail.com>
2023-05-13 15:01:00 +0100

    search bar added

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //You can edit ALL of the code here
+
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
@@ -50,3 +51,75 @@ function makePageForEpisodes(episodeList) {
 }
 
 window.onload = setup;
+
+// second commit - 200 points
+
+function makePageForEpisodes(episodeList) {
+  const rootElem = document.getElementById("root");
+
+  // Add search input
+  const searchInput = document.createElement("input");
+  searchInput.type = "text";
+  searchInput.placeholder = "Search episodes";
+  searchInput.addEventListener("input", () =>
+    updateDisplayedEpisodes(episodeList, searchInput.value.trim())
+  );
+  rootElem.appendChild(searchInput);
+
+  // Add episode count display
+  const episodeCountElem = document.createElement("p");
+  episodeCountElem.id = "episode-count";
+  rootElem.appendChild(episodeCountElem);
+
+  // Add episode list container
+  const episodeListContainer = document.createElement("div");
+  episodeListContainer.id = "episode-list-container";
+  rootElem.appendChild(episodeListContainer);
+
+  updateDisplayedEpisodes(episodeList);
+}
+
+function updateDisplayedEpisodes(episodeList, searchTerm = "") {
+  const episodeListContainer = document.getElementById(
+    "episode-list-container"
+  );
+  episodeListContainer.innerHTML = "";
+
+  const matchingEpisodes = episodeList.filter((episode) => {
+    const summary = episode.summary.toLowerCase();
+    const name = episode.name.toLowerCase();
+    return (
+      summary.includes(searchTerm.toLowerCase()) ||
+      name.includes(searchTerm.toLowerCase())
+    );
+  });
+
+  const episodeCountElem = document.getElementById("episode-count");
+  episodeCountElem.textContent = `Displaying ${matchingEpisodes.length} of ${episodeList.length} episodes`;
+
+  matchingEpisodes.forEach((episode) => {
+    const episodeElem = document.createElement("div");
+
+    const episodeCode = `S${episode.season
+      .toString()
+      .padStart(2, "0")}E${episode.number.toString().padStart(2, "0")}`;
+    const episodeTitle = `${episodeCode} - ${episode.name}`;
+
+    const episodeImg = document.createElement("img");
+    episodeImg.src = episode.image.medium;
+    episodeImg.alt = episodeTitle;
+
+    const episodeSummary = document.createElement("p");
+    episodeSummary.innerHTML = episode.summary;
+
+    episodeElem.appendChild(episodeImg);
+    episodeElem.appendChild(document.createElement("br"));
+    episodeElem.appendChild(document.createTextNode(episodeTitle));
+    episodeElem.appendChild(document.createElement("br"));
+    episodeElem.appendChild(
+      document.createTextNode(episodeSummary.textContent)
+    );
+
+    episodeListContainer.appendChild(episodeElem);
+  });
+}
